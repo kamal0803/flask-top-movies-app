879e9fcd733753b8ad01cbe7ac651826de396926
Kamal Rajan Israni <kamal0803@gmail.com>
2024-08-07 19:08:57 +0530

    Add movie functionality implemented partially using TMDB API

--- main.py
+++ main.py
@@ -33,35 +33,43 @@ db = SQLAlchemy(model_class=Base)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///top-movies-collection.db"
 db.init_app(app)
 
+search_movie_url = "https://api.themoviedb.org/3/search/movie"
+movie_details_url = "https://api.themoviedb.org/3/movie/"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMGNiMjg4MjMyYWM4NWI3MTllN2E2NzY4MzE4MGIwMCIsIm5iZiI6MTcyMzAyMjc1OC42MzU2LCJzdWIiOiI2NmIzMzg1MmNjOWNkYjNhOWU3YjkxZTIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.H0zyhikFIxSAM2w6z4BXHpHBuNg_P8a3iiJnZIAYoPc",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+}
 
 # CREATE TABLE
 class Movie(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
-    year: Mapped[int] = mapped_column(Integer)
-    description: Mapped[str] = mapped_column(String(250), nullable=False)
-    rating: Mapped[float] = mapped_column(Float, nullable=False)
-    ranking: Mapped[float] = mapped_column(Float, nullable=False)
-    review: Mapped[str] = mapped_column(String(250), nullable=False)
-    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=True)
+    year: Mapped[int] = mapped_column(Integer, nullable=True)
+    description: Mapped[str] = mapped_column(String(250), nullable=True)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[float] = mapped_column(Float, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=True)
 
 
 
 with app.app_context():
     db.create_all()
 
-with app.app_context():
-    second_movie = Movie(
-        title="Avatar The Way of Water",
-        year=2022,
-        description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
-        rating=7.3,
-        ranking=9,
-        review="I liked the water.",
-        img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
-    )
-    db.session.add(second_movie)
-    db.session.commit()
+# with app.app_context():
+#     second_movie = Movie(
+#         title="Avatar The Way of Water",
+#         year=2022,
+#         description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#         rating=7.3,
+#         ranking=9,
+#         review="I liked the water.",
+#         img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+#     )
+#     db.session.add(second_movie)
+#     db.session.commit()
 
 
 @app.route("/")
@@ -89,7 +97,10 @@ def edit():
             movie_to_update.review = review
             db.session.commit()
 
-        return render_template("index.html")
+            result = db.session.execute(db.select(Movie).order_by(Movie.title))
+            all_movies = result.scalars().all()
+
+        return render_template("index.html", all_movies=all_movies)
     return render_template("edit.html", form=form)
 
 @app.route("/delete")
@@ -114,9 +125,33 @@ def add():
     if form.validate_on_submit():
         movie_title = form.movie_title.data
 
-        return render_template("index.html")
+        params = {
+            "query": movie_title
+        }
+
+        all_movies_response = requests.get(search_movie_url, headers=headers, params=params).json()['results']
+
+        return render_template("select.html", all_movies_response=all_movies_response)
 
     return render_template("add.html", form=form)
+
+@app.route("/search", methods=["GET", "POST"])
+def search_new_movie():
+
+    id = request.args.get('id')
+    response = requests.get(movie_details_url + id, headers=headers).json()
+
+    title = response['title']
+    img_url = "https://image.tmdb.org/t/p/w500" + response['poster_path']
+    year = int(response['release_date'].split("-")[0])
+    description = response['overview']
+
+    new_movie = Movie(title=title, img_url=img_url, year=year, description=description)
+    db.session.add(new_movie)
+    db.session.commit()
+
+
+    return redirect(url_for("edit", id=new_movie.id))
 
 
 if __name__ == '__main__':
